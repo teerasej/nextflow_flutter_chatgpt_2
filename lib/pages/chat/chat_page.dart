331ee7f78e227a0776537242bb09a2d7efb34283
Teerasej <teerasej@gmail.com>
2025-02-13 22:22:54 +0700

    add chat page

---
+++ lib/pages/chat/chat_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'chat_history_component.dart';
+import 'chat_input_component.dart';
+
+class ChatPage extends StatelessWidget {
+  const ChatPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chat Page'),
+      ),
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
