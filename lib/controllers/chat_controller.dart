9cc6c379cda3d969e8908ca5810eea05bff5135a
Teerasej <teerasej@gmail.com>
2025-02-13 23:49:28 +0700

    Add a list of chat message model with sample data. also make sending method put new message into the list

--- lib/controllers/chat_controller.dart
+++ lib/controllers/chat_controller.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:nextflow_chatgpt/models/chat_message.dart';
 
 class ChatController extends GetxController {
   final TextEditingController textController = TextEditingController();
+  var messages = <ChatMessage>[
+    ChatMessage(message: "Hello, how can I help you?", isSender: false),
+    ChatMessage(message: "I need some information about your services.", isSender: true)
+  ].obs;
 
   void handleSend() {
     String inputValue = textController.text;
-    print(inputValue); 
-    textController.clear(); 
+    if (inputValue.isNotEmpty) {
+      messages.add(ChatMessage(message: inputValue, isSender: true));
+      textController.clear();
+    }
   }
 
 }
