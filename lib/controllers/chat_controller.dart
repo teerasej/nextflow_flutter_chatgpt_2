4903d65358cb4d22c29c406d2b90d4c7f522b650
Teerasej <teerasej@gmail.com>
2025-02-14 00:12:05 +0700

    add dio and make request to ai to get response to put in chat history

--- lib/controllers/chat_controller.dart
+++ lib/controllers/chat_controller.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:nextflow_chatgpt/models/chat_message.dart';
@@ -6,15 +7,59 @@ class ChatController extends GetxController {
   final TextEditingController textController = TextEditingController();
   var messages = <ChatMessage>[
     ChatMessage(message: "Hello, how can I help you?", isSender: false),
-    ChatMessage(message: "I need some information about your services.", isSender: true)
+    ChatMessage(
+        message: "I need some information about your services.", isSender: true)
   ].obs;
 
-  void handleSend() {
+  final Dio _dio = Dio();
+  final String _azureOpenAiEndpoint = '';
+  final String _apiKey = '';
+
+  void handleSend() async {
     String inputValue = textController.text;
     if (inputValue.isNotEmpty) {
       messages.add(ChatMessage(message: inputValue, isSender: true));
       textController.clear();
+      await sendMessageToAzureOpenAi(inputValue);
     }
   }
 
+  Future<void> sendMessageToAzureOpenAi(String message) async {
+    try {
+      final response = await _dio.post(
+        _azureOpenAiEndpoint,
+        options: Options(
+          headers: {
+            'Content-Type': 'application/json',
+            'api-key': _apiKey
+          },
+        ),
+        data: {
+          "messages": [
+            {
+              "role": "system",
+              "content":
+                  "You are an AI assistant that helps people find information."
+            },
+            {
+              "role": "user",
+              "content": message,
+            },
+          ],
+          "temperature": 0.7,
+          "max_tokens": 800,
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final responseMessage = response.data['choices'][0]['message']['content'];
+        messages.add(ChatMessage(message: responseMessage, isSender: false));
+      } else {
+        messages.add(ChatMessage(
+            message: 'Error: ${response.statusMessage}', isSender: false));
+      }
+    } catch (e) {
+      messages.add(ChatMessage(message: 'Error: $e', isSender: false));
+    }
+  }
 }
